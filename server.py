1936ca04cfce5fc7f438def695bc9fda9919d88f
Muhammad Ojagzada <85498539+MuhammadOo@users.noreply.github.com>
2022-07-29 21:26:46 +0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,20 @@
+from concurrent.futures import thread
+from tkinter import N
+from server_app import pred
+from threading import Thread
+from flask import Flask
+
+app = Flask(__name__)
+thread = Thread(target = pred)
+
+@app.route("/")
+
+def run_server():
+    if not run_server():
+        thread.start()
+    else:
+        pass
+    return 'Server is running...'
+
+if __name__== '__main__':
+    app.run(debug=True)
