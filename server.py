1a2b985c0be3d3a3c0ff6428be6b94b5c5f6cddb
Muhammad Ojagzada <85498539+MuhammadOo@users.noreply.github.com>
2022-08-05 14:26:51 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from server_app import pred
+from server_app import app_func
 from threading import Thread
 from flask import Flask
 
+thread = Thread(target = app_func)
+
 app = Flask(__name__)
-thread = Thread(target = pred)
 
 @app.route("/")
-
 def run_server():
-    if not run_server():
+    if not thread.is_alive():
         thread.start()
     else:
         pass
